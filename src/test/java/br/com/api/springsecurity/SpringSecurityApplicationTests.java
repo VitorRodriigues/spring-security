3da4dd69aaeeb55c8d657c886a96bd69fbbdde77
Vitor Rodrigues <tdvse@hotmail.com>
2022-09-14 16:00:18 -0300

    Initial commit

---
+++ src/test/java/br/com/api/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
